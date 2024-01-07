a70d02464e033eb21fcba91f8a63bd41046c2963
sathish <155909016+sathishkumarsf@users.noreply.github.com>
2024-01-08 00:08:51 +0530

    Add files via upload

---
+++ FindNumber.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int value=45;
+		
+		if (value>0) 
+		{
+			System.out.println("The number is positive");
+		} else {
+
+			System.out.println("The number is negative");
+		}
+
+	}
+
+}
